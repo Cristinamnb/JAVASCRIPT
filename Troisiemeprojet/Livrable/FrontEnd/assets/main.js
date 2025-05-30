32407eadf0cc7303f5a77eb86debb3ca4eaae04a
cristinamnb <contactcristinaneto@gmail.com>
2025-05-30 10:29:00 -0400

    Ajout filtres

--- Troisiemeprojet/Livrable/FrontEnd/assets/main.js
+++ Troisiemeprojet/Livrable/FrontEnd/assets/main.js
@@ -11,6 +11,9 @@ async function fetchWorks() {
     works.forEach(work => {
       const figure = document.createElement("figure");
 
+      // Ajoute les classes nécessaires pour le filtrage
+      figure.classList.add("work-item", `category-id-${work.categoryId}`);
+      
       const img = document.createElement("img");
       img.src = work.imageUrl;
       img.alt = work.title;
@@ -31,3 +34,59 @@ async function fetchWorks() {
 
 // 4. Appelle la fonction pour afficher les travaux
 fetchWorks();
+
+// Ajout de filtres de catégories pour filtrer les œuvres dans la galerie
+fetch("http://localhost:5678/api/categories")
+  .then(function(response) {
+    if(response.ok) {
+      return response.json();
+    }
+  })
+  .then(function(data) {
+    let categories = data;
+    categories.unshift({id: 0, name: 'Tous'}); // Ajoute l'option "Tous" au début
+
+    categories.forEach((category) => {
+      // Création d'un bouton
+      let myButton = document.createElement('button');
+      myButton.classList.add('work-filter', 'filters-design');
+      if(category.id === 0) myButton.classList.add('filter-active', 'filter-all');
+      myButton.setAttribute('data-filter', category.id);
+      myButton.textContent = category.name;
+
+      // Ajout du bouton dans la div.filters
+      document.querySelector("div.filters").appendChild(myButton);
+
+      // Événement de clic sur le bouton
+      myButton.addEventListener('click', function(event) {
+        event.preventDefault();
+
+        // Mise à jour des classes actives
+        document.querySelectorAll('.work-filter').forEach((workFilter) => {
+          workFilter.classList.remove('filter-active');
+        });
+        event.target.classList.add('filter-active');
+
+        // Récupération de la catégorie cliquée
+        let categoryId = myButton.getAttribute('data-filter');
+
+        // Filtrage des éléments
+        document.querySelectorAll('.work-item').forEach(workItem => {
+          if (categoryId === "0") {
+            // Affiche tout
+            workItem.style.display = 'block';
+          } else {
+            // Affiche uniquement ceux de la bonne catégorie
+            if (workItem.classList.contains(`category-id-${categoryId}`)) {
+              workItem.style.display = 'block';
+            } else {
+              workItem.style.display = 'none';
+            }
+          }
+        });
+      });
+    });
+  })
+  .catch(function(err) {
+    console.log("Erreur lors du chargement des catégories :", err);
+  });
